df98e4b6c83508019291151a96c433c3ef57039f
Benjamin <benjamin.kinnard1@gmail.com>
2017-04-14 16:44:02 +0100

    passed two player winChecker

--- card_game/WinChecker.java
+++ card_game/WinChecker.java
@@ -5,10 +5,10 @@ import java.util.*;
 
 public class WinChecker {
 
-  public String winner() {
-    return "abc";
+  public String winner(Player hand1, Player hand2) {
+    if(hand1.sumHand() > hand2.sumHand() ) {
+      return "player1";
+      }
+      return "player2";
   }
-  // for player in players
-  //   int playerTotal = player.sumHand
-  // players.sort
 }
--- card_tests/WinCheckerTest.java
+++ card_tests/WinCheckerTest.java
@@ -7,15 +7,30 @@ import java.util.*;
 public class WinCheckerTest {
 
   WinChecker winChecker;
+  Player player1;
+  Player player2;
+  Card card1;
+  Card card2;
+  Card card3;
+  Card card4;
 
   @Before
   public void before(){
     winChecker = new WinChecker();
+    player1 = new Player("Ben");
+    player2 = new Player("dave");
+    card1 = new Card(10, Suit.Spades);
+    card2 = new Card(9, Suit.Spades);
+    card3 = new Card(5, Suit.Spades);
+    card4 = new Card(3, Suit.Spades);
+    player2.setHand(card1, card2);
+    player1.setHand(card3, card4);
+
   }
 
   @Test
-  public void checkWinnerSaysABC() {
-    assertEquals("abc", winChecker.winner() );
+  public void checkWinner() {
+    assertEquals("player2", winChecker.winner(player1, player2));
   }
 
 }
